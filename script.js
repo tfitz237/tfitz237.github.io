76a33de443c77cda61e68f1e7586ac50f091fec1
Tom Fitzpatrick <tfitz237@gmail.com>
2016-08-30 07:16:19 +0000

    Upgraded scrolling, changed layout of nav/body/footer to flexbox to span the height of the page properly.

---
+++ script.js
@@ -0,0 +1,27 @@
+            $(document).ready(function() {
+                var scroll_left = 0;
+                var currentPage = $('.content-about');
+                var currentLink = $('.navbar-nav li:first-child');
+                if(currentPage.height() > $('.body').height()) {
+                    $('.body').css('overflow-y', 'scroll');
+                } else {
+                    $('.body').css('overflow-y', 'hidden');
+                }
+                $('.link-nav').click(function(event) {
+                    if($(this) == currentLink) return false;
+                    currentPage =  $('.content-' + $(this).attr('data-link'));
+                    scroll_left = currentPage.position().left;
+                    if(currentPage.height() < $('.body').height()) {
+                        $('.body').css('overflow-y', 'hidden');
+                    }
+                    $('.body').animate({scrollLeft:scroll_left}, 800, function() {
+                        if(currentPage.height() > $('.body').height()) {
+                            $('.body').css('overflow-y', 'scroll');
+                        }                     
+                    });
+                    $(currentLink).removeClass('active');
+                    currentLink = $(this);
+                    $(this).addClass('active');
+                    return false;
+                });
+            });
