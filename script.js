d2baa6d0bda04eff2642a448ebc0597ba7237a7a
Tom Fitzpatrick <tfitz237@gmail.com>
2016-08-30 22:43:11 +0000

    URL now changes, and allows for direct links to specific pages.

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-            $(document).ready(function() {
-                var scroll_left = 0;
-                var currentPage = $('.content-about');
-                var currentLink = $('.navbar-nav li:first-child');
-                if(currentPage.height() > $('.body').height()) {
-                    $('.body').css('overflow-y', 'scroll');
-                } else {
-                    $('.body').css('overflow-y', 'hidden');
-                }
-                $('.link-nav').click(function(event) {
-                    if($(this) == currentLink) return false;
-                    currentPage =  $('.content-' + $(this).attr('data-link'));
-                    scroll_left = currentPage.position().left;
-                    if(currentPage.height() < $('.body').height()) {
-                        $('.body').css('overflow-y', 'hidden');
-                    }
-                    $('.navbar-toggle').click();
-                    $('.body').animate({scrollLeft:scroll_left}, 800, function() {
-                        if(currentPage.height() > $('.body').height()) {
-                            $('.body').css('overflow-y', 'scroll');
-                        }
-                        
-                    });
-                    $(currentLink).removeClass('active');
-                    currentLink = $(this);
-                    $(this).addClass('active');
-                    return false;
-                });
-            });
+$(document).ready(function() {
+    var hash = window.location.hash.slice(1);
+    console.log(hash);
+    var scroll_left = 0;
+    var currentPage = $('.content-about');
+    var currentLink = $('.navbar-nav li:first-child');
+    if(hash.length > 0) {
+        currentPage = $('.content-' + hash);
+        scroll_left = currentPage.position().left;
+        $('.body').animate({scrollLeft:scroll_left}, 800);
+    }
+    if(currentPage.height() > $('.body').height()) {
+        $('.body').css('overflow-y', 'scroll');
+    } else {
+        $('.body').css('overflow-y', 'hidden');
+    }
+    $(".link-nav[data-link='"+hash+"']").click();
+    $('.link-nav').click(function(event) {
+        if($(this) == currentLink) return false;
+        currentPage =  $('.content-' + $(this).attr('data-link'));
+        scroll_left = currentPage.position().left;
+        if(currentPage.height() < $('.body').height()) {
+            $('.body').css('overflow-y', 'hidden');
+        }
+        $('.navbar-toggle').click();
+        $('.body').animate({scrollLeft:scroll_left}, 800, function() {
+            if(currentPage.height() > $('.body').height()) {
+                $('.body').css('overflow-y', 'scroll');
+            }
+            
+        });
+        $(currentLink).removeClass('active');
+        currentLink = $(this);
+        $(this).addClass('active');
+        //return false;
+    });
+});
