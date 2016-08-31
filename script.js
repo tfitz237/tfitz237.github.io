5718f2f9bc3d078eb147db9aee9bcde59793ca2d
Tom Fitzpatrick <tfitz237@gmail.com>
2016-08-31 01:34:08 +0000

    Can now swipe left/right on mobile.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     var hash = window.location.hash.slice(1);
-    console.log(hash);
     var scroll_left = 0;
     var currentPage = $('.content-about');
     var currentLink = $('.navbar-nav li:first-child');
@@ -36,6 +35,48 @@ $(document).ready(function() {
         currentLink = $(this);
         $(this).addClass('active');
         //return false;
+    });
+    var body = document.getElementsByClassName('body')[0];
+    var ht = new Hammer(body);
+    ht.on('swipeleft', function() {
+       currentPage = (currentPage.next().length > 0) ? currentPage.next() : currentPage;
+        scroll_left = currentPage.position().left;
+        if(currentPage.height() < $('.body').height()) {
+            $('.body').css('overflow-y', 'hidden');
+        }
+        if($('.navbar-collapse').css('display') != "none") {
+            $('.navbar-toggle').click();
+        }
+        $('.body').animate({scrollLeft:scroll_left, scrollTop: 0}, 800, function() {
+            if(currentPage.height() > $('.body').height()) {
+                $('.body').css('overflow-y', 'scroll');
+                
+            }
+            
+        });
+        currentLink.removeClass('active');
+        currentLink = (currentLink.next().length > 0) ? currentLink.next() : currentLink;
+        currentLink.addClass('active');
+    });
+    ht.on('swiperight', function() {
+       currentPage = (currentPage.prev().length > 0) ? currentPage.prev() : currentPage;
+        scroll_left = currentPage.position().left;
+        if(currentPage.height() < $('.body').height()) {
+            $('.body').css('overflow-y', 'hidden');
+        }
+        if($('.navbar-collapse').css('display') != "none") {
+            $('.navbar-toggle').click();
+        }
+        $('.body').animate({scrollLeft:scroll_left, scrollTop: 0}, 800, function() {
+            if(currentPage.height() > $('.body').height()) {
+                $('.body').css('overflow-y', 'scroll');
+                
+            }
+            
+        });
+        currentLink.removeClass('active');
+        currentLink = (currentLink.prev().length > 0) ? currentLink.prev() : currentLink;
+        currentLink.addClass('active');
     });
     $(window).on('resize', function() {
        if(currentPage.position().left != $('.body').scrollLeft()) {
